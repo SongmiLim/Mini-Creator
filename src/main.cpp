d0446c62a1e466c1da545b7eb0e427fe195157cf
songmiLim <lsmi00121@gmail.com>
2024-03-19 17:54:52 +0900

    add shader class

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,35 @@
+#include "common.h"
+#include "shader.h"
+
 #include <spdlog/spdlog.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(int argc, const char **argv)
-{
+void OnFramebufferSizeChange(GLFWwindow *window, int width, int height) {
+    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow *window, int key, int scancode, int action, int mods) {
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+                key, scancode,
+                action == GLFW_PRESS ? "Pressed" : action == GLFW_RELEASE ? "Released"
+                                               : action == GLFW_REPEAT    ? "Repeat"
+                                                                          : "Unknown",
+                mods & GLFW_MOD_CONTROL ? "C" : "-",
+                mods & GLFW_MOD_SHIFT ? "S" : "-",
+                mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main(int argc, const char **argv) {
     SPDLOG_INFO("Start program");
 
     // initialize glfw
     SPDLOG_INFO("Initialize glfw");
-    if (!glfwInit())
-    {
+    if (!glfwInit()) {
         const char *description = nullptr;
         glfwGetError(&description);
         SPDLOG_ERROR("failed to initialize glfw: {}", description);
@@ -23,10 +43,8 @@ int main(int argc, const char **argv)
 
     // create glfw window
     SPDLOG_INFO("Create glfw window");
-    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
-                                   nullptr, nullptr);
-    if (!window)
-    {
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window) {
         SPDLOG_ERROR("failed to create glfw window");
         glfwTerminate();
         return -1;
@@ -34,9 +52,8 @@ int main(int argc, const char **argv)
 
     glfwMakeContextCurrent(window);
 
-    // glad를 활용한 OpenGL 함수 로딩
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
+    // loading openGL function using glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         SPDLOG_ERROR("failed to initialize glad");
         glfwTerminate();
         return -1;
@@ -44,10 +61,20 @@ int main(int argc, const char **argv)
     // auto glVersion = glGetString(GL_VERSION);
     // SPDLOG_INFO("OpenGL context version: {}", glVersion);
 
+    auto vertexShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
+    auto fragmentShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    SPDLOG_INFO("vertex shader id: {}", vertexShader->Get());
+    SPDLOG_INFO("fragment shader id: {}", fragmentShader->Get());
+
+    glfwSetWindowPos(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
+
     // start glfw loop
     SPDLOG_INFO("Start main loop");
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
     }
 
@@ -55,3 +82,4 @@ int main(int argc, const char **argv)
 
     return 0;
 }
+
