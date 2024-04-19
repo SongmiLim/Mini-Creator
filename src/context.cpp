d8e65cfbf0f68d72b1849eeb5610c0163089a85b
songmiLim <lsmi00121@gmail.com>
2024-04-19 17:59:12 +0900

    feat: Add UI for camera controls

--- src/context.cpp
+++ src/context.cpp
@@ -96,8 +96,19 @@ bool Context::Init() {
 }
 
 void Context::Render() {
-    if (ImGui::Begin("my first ImGui window")) {
-        ImGui::Text("This is first text...");
+    if (ImGui::Begin("ui window")) {
+        ImGui::DragFloat3("camera pos", glm::value_ptr(m_cameraPos), 0.01f);
+        ImGui::DragFloat("camera Fov", &m_cameraFov, 0.5f, 1.0f, 45.0f);
+        ImGui::DragFloat("camera yaw", &m_cameraYaw, 0.5f);
+        ImGui::DragFloat("camera pitch", &m_cameraPitch, 0.5f, -89.0f, 89.0f);
+        ImGui::Separator();
+
+        if(ImGui::Button("reset camera")) {
+            m_cameraYaw = 0.0f;
+            m_cameraPitch = 0.0f;
+            m_cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+            m_cameraFov = 45.0f;
+        }
     }
     ImGui::End();
 
