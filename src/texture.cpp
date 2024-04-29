296330da288868ffb515a587f3ba89e470fe93ee
songmiLim <lsmi00121@gmail.com>
2024-04-29 19:12:47 +0900

    Refactor: Refactor Texture class to support frame buffer

--- src/texture.cpp
+++ src/texture.cpp
@@ -7,6 +7,14 @@ TextureUPtr Texture::CreateFromImage(const Image* image) {
     return std::move(texture);
 }
 
+TextureUPtr Texture::Create(int width, int height, uint32_t format) {
+    auto texture = TextureUPtr(new Texture());
+    texture->CreateTexture();
+    texture->SetTextureFormat(width, height, format);
+    texture->SetFilter(GL_LINEAR, GL_LINEAR);
+    return std::move(texture);
+}
+
 Texture::~Texture() {
     if (m_texture) {
         glDeleteTextures(1, &m_texture);
@@ -43,11 +51,21 @@ void Texture::SetTextureFromImage(const Image* image) {
         case 2: format = GL_RG; break;
         case 3: format = GL_RGB; break;
     }
-    
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
-        image->GetWidth(), image->GetHeight(), 0,
-        format, GL_UNSIGNED_BYTE,
-        image->GetData());
+    m_width = image->GetWidth();
+    m_height = image->GetHeight();
+    m_format = format;
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, m_width, m_height, 
+        0, format, GL_UNSIGNED_BYTE, image->GetData());
     
     glGenerateMipmap(GL_TEXTURE_2D);
+}
+
+void Texture::SetTextureFormat(int width, int height, uint32_t format) {
+    m_width = width;
+    m_height = height;
+    m_format = format;
+
+    glTexImage2D(GL_TEXTURE_2D, 0, m_format, m_width, m_height, 
+        0, m_format, GL_UNSIGNED_BYTE, nullptr);
 }
