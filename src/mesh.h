ff9d2fcc157876d079d41e3def791ad9bf652fce
songmiLim <lsmi00121@gmail.com>
2024-04-29 19:15:57 +0900

    feat: Add plane mesh

--- src/mesh.h
+++ src/mesh.h
@@ -35,7 +35,8 @@ class Mesh {
     static MeshUPtr Create(const std::vector<Vertex>& vertices, 
       const std::vector<uint32_t>& indices, uint32_t primitiveType);
     static MeshUPtr CreateBox();
-
+    static MeshUPtr CreatePlane();
+    
     const VertexLayout* GetVertexLayout() const {
         return m_vertexLayout.get();
     }
